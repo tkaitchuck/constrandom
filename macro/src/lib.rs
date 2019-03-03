1b8b6e4d413a92f80e24336d2fc89f6a958e3c92
Tom <Tom.Kaitchuck@dell.com>
2019-03-02 23:20:36 -0800

    Remove the need for macro hygiene

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -1,39 +1,27 @@
-#![feature(proc_macro_hygiene)]
-
 extern crate proc_macro;
 use proc_macro::TokenStream;
 
 #[proc_macro]
 pub fn const_random_u8(_input: TokenStream) -> TokenStream {
-    let value: u8 = rand::random();
-    let out_str = format!("0x{:x}",value);
-    out_str.parse().unwrap()
+    format!("0x{:x}", rand::random::<u8>()).parse().unwrap()
 }
 
 #[proc_macro]
 pub fn const_random_u16(_input: TokenStream) -> TokenStream {
-    let value: u16 = rand::random();
-    let out_str = format!("0x{:x}",value);
-    out_str.parse().unwrap()
+    format!("0x{:x}", rand::random::<u16>()).parse().unwrap()
 }
 
 #[proc_macro]
 pub fn const_random_u32(_input: TokenStream) -> TokenStream {
-    let value: u32 = rand::random();
-    let out_str = format!("0x{:x}",value);
-    out_str.parse().unwrap()
+    format!("0x{:x}", rand::random::<u32>()).parse().unwrap()
 }
 
 #[proc_macro]
 pub fn const_random_u64(_input: TokenStream) -> TokenStream {
-    let value: u64 = rand::random();
-    let out_str = format!("0x{:x}",value);
-    out_str.parse().unwrap()
+    format!("0x{:x}", rand::random::<u64>()).parse().unwrap()
 }
 
 #[proc_macro]
 pub fn const_random_u128(_input: TokenStream) -> TokenStream {
-    let value: u128 = rand::random();
-    let out_str = format!("0x{:x}",value);
-    out_str.parse().unwrap()
+    format!("0x{:x}", rand::random::<u128>()).parse().unwrap()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,13 @@
-#![feature(proc_macro_hygiene)]
-use const_random_macro;
+//use const_random_macro;
 
 /// # Random constants
 /// Allows you to insert random constants into your code that will be auto-generated at compile time.
 /// A new value will be generated every time the relevent file is re-built.
 /// # Example
 /// ```
-/// #![feature(proc_macro_hygiene)]
 /// use const_random::const_random  ;
 /// const MY_RANDOM_NUMBER: u32 = const_random!(u32);
 /// ```
-/// Note that the `#![feature(proc_macro_hygiene)]` must be enabled on your crate for the macro to work.
 ///
 /// The following types are supported u8, u16, u32, u64, and u128
 ///
