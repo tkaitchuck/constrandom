4a53c19b51ab68b9fb6d734b02a77fe287fc3751
David Tolnay <dtolnay@gmail.com>
2020-03-27 16:19:57 -0700

    Resolve non_upper_case_globals lints in test suite

--- tests/tests.rs
+++ tests/tests.rs
@@ -2,28 +2,28 @@ use const_random::const_random;
 
 #[test]
 fn u32() {
-    const value1: u32 = const_random!(u32);
-    const value2: u32 = const_random!(u32);
-    assert_ne!(0, value1);
-    assert_ne!(0, value2);
-    assert_ne!(value1, value2);
+    const VALUE1: u32 = const_random!(u32);
+    const VALUE2: u32 = const_random!(u32);
+    assert_ne!(0, VALUE1);
+    assert_ne!(0, VALUE2);
+    assert_ne!(VALUE1, VALUE2);
 }
 
 #[test]
 fn i64() {
-    const value1: i64 = const_random!(i64);
-    const value2: i64 = const_random!(i64);
-    assert_ne!(0, value1);
-    assert_ne!(0, value2);
-    assert_ne!(value1, value2);
+    const VALUE1: i64 = const_random!(i64);
+    const VALUE2: i64 = const_random!(i64);
+    assert_ne!(0, VALUE1);
+    assert_ne!(0, VALUE2);
+    assert_ne!(VALUE1, VALUE2);
 }
 
 
 #[test]
 fn u128() {
-    const value1: u128 = const_random!(u128);
-    const value2: u128 = const_random!(u128);
-    assert_ne!(0, value1);
-    assert_ne!(0, value2);
-    assert_ne!(value1, value2);
+    const VALUE1: u128 = const_random!(u128);
+    const VALUE2: u128 = const_random!(u128);
+    assert_ne!(0, VALUE1);
+    assert_ne!(0, VALUE2);
+    assert_ne!(VALUE1, VALUE2);
 }
